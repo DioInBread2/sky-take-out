218da262e6aca5cc942add9db6ef75ecc7e82c67
DioInBread <1348402900@qq.com>
2025-03-24 02:28:16 +0800

    新增员工

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +28,22 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 处理SQL异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(SQLIntegrityConstraintViolationException e) {
+        log.error("服务器发生异常：{}", e);
+        String message = e.getMessage();
+        if (message.contains("Duplicate entry")) {
+            int i = message.indexOf("Duplicate entry");
+            String errMsg = message.substring(i);
+            String[] arr = errMsg.split(" ");
+            return Result.error(arr[2] + MessageConstant.ALREADY_EXISTS);
+        } else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
