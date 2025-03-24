e693f9f2b7e80b18ecbb1d2c22e24ff06ada97ac
DioInBread <1348402900@qq.com>
2025-03-25 01:28:18 +0800

    2025.3.24 init
    新增员工
    
    员工分页查询
    
    编辑员工
    
    公共字段自动填充
    
    新增菜品
    
    菜品分页查询
    
    删除菜品

--- sky-common/src/main/java/com/sky/utils/AliOssUtil.java
+++ sky-common/src/main/java/com/sky/utils/AliOssUtil.java
@@ -52,17 +52,9 @@ public class AliOssUtil {
             }
         }
 
-        //文件访问路径规则 https://BucketName.Endpoint/ObjectName
-        StringBuilder stringBuilder = new StringBuilder("https://");
-        stringBuilder
-                .append(bucketName)
-                .append(".")
-                .append(endpoint)
-                .append("/")
-                .append(objectName);
+        String path = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + objectName;
+        log.info("文件上传到:{}", path);
 
-        log.info("文件上传到:{}", stringBuilder.toString());
-
-        return stringBuilder.toString();
+        return path;
     }
 }
