55dd9c23fde50f9bbdb982faee7f926e9a9b7218
DioInBread <1348402900@qq.com>
2025-03-26 01:38:14 +0800

    2025.3.26 商品浏览
    缓存菜品
    
    SpringCache\ 添加购物车
    
    查看购物车
    
    清空购物车
    
    地址
    
    用户下单
    
    微信支付

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,12 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.enumeration.OperationType;
+import com.sky.vo.DishItemVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,4 +19,19 @@ public interface SetmealDishMapper {
      * @return
      */
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+
+    /**
+     * 批量插入关系表
+     * @param setmealDishes
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insertBatch(List<SetmealDish> setmealDishes);
+
+
+    /**
+     * 根据套餐删除关联关系
+     * @param setmealIds
+     */
+    void deleteBySetmealIds(List<Long> setmealIds);
 }
